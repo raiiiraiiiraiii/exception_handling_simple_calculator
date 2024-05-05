355b79ae6b8429cf8c0e8da051362bf0e93795ef
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-05 22:05:06 +0800

    Add Pseudocode (2)

---
+++ exception_handling_simple_calculator.py
@@ -0,0 +1,15 @@
+#Pseudocode
+
+# Short Message
+# Ask the user to choose one of the four math operations
+    # 1.) Addition
+    # 2.) Subtraction
+    # 3.) Multiplication
+    # 4.) Division
+# Check if the inputted option is within range or a number
+# Ask the user for two numbers
+# Check if what the user inputted is valid
+# Display the result
+# Ask the user if the user wants to try again or not
+    # If yes, the program will repeat
+    # If no, the program will exit
