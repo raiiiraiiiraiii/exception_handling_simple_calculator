523eb8aa61885fce446960e551543d0543d5c09d
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-05 22:29:59 +0800

    Added Message, Options, etc. (1)

--- exception_handling_simple_calculator.py
+++ exception_handling_simple_calculator.py
@@ -1,15 +1,36 @@
 #Pseudocode
 
 # Short Message
+print('='*50)
+print('\nWelcome to Simple Calculator in Python!\n')
+
 # Ask the user to choose one of the four math operations
     # 1.) Addition
     # 2.) Subtraction
     # 3.) Multiplication
     # 4.) Division
+
+while True:
+    print("Choose an operator from the list (1-4): \n"
+          "1. Addition (➕)\n"
+          "2. Subtraction (➖)\n"
+          "3. Multiplication (✖️)\n"
+          "4. Division (➗)\n")
+
+
+    chosen_option = int(input("Enter your chosen operation: "))
+
+
 # Check if the inputted option is within range or a number
+
 # Ask the user for two numbers
+first_number = float(input('Enter your first number: '))
+second_number = float(input('Enter your second number: '))
+
 # Check if what the user inputted is valid
+
 # Display the result
+
 # Ask the user if the user wants to try again or not
     # If yes, the program will repeat
     # If no, the program will exit
