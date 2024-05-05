4be7705b6f3e3bfb3720e5a5578acd97bd9b5491
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-05-06 00:05:02 +0800

    Validate Inputs (1)

--- exception_handling_simple_calculator.py
+++ exception_handling_simple_calculator.py
@@ -2,7 +2,7 @@
 
 # Short Message
 print('='*50)
-print('\nWelcome to Simple Calculator in Python!\n')
+print('\nWelcome to Simple Calculator in Python!')
 
 # Ask the user to choose one of the four math operations
     # 1.) Addition
@@ -11,23 +11,33 @@ print('\nWelcome to Simple Calculator in Python!\n')
     # 4.) Division
 
 while True:
-    print("Choose an operator from the list (1-4): \n"
-          "1. Addition (➕)\n"
-          "2. Subtraction (➖)\n"
-          "3. Multiplication (✖️)\n"
-          "4. Division (➗)\n")
-
-
-    chosen_option = int(input("Enter your chosen operation: "))
-
+    print("\nChoose an operator from the list (1-4): \n"
+          " 1. Addition (➕)\n"
+          " 2. Subtraction (➖)\n"
+          " 3. Multiplication (✖️)\n"
+          " 4. Division (➗)\n"
+          " 5. Exit\n")
 
 # Check if the inputted option is within range or a number
+    try:
+        chosen_option = int(input("Enter your chosen operation: "))
+        if chosen_option not in range (1, 4+1):
+            print('‼️ Input out of range. Enter numbers 1 to 5 only. ‼️')
+            continue
+        if chosen_option == 5:
+            break
 
-# Ask the user for two numbers
-first_number = float(input('Enter your first number: '))
-second_number = float(input('Enter your second number: '))
+    except ValueError:
+        print('Input invalid. Enter a valid number.')
+        continue
 
-# Check if what the user inputted is valid
+# Ask the user for two numbers and validate input
+    try:
+        first_number = float(input('Enter your first number: '))
+        second_number = float(input('Enter your second number: '))
+    except ValueError:
+        print('Input invalid. Enter a valid number.')
+        continue
 
 # Display the result
 
